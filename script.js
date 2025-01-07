2bf9ff69682a5b6e79c370ce0e3852c600f227b7
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-07 15:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function findDOMLevel(){
+	let element=document.getElementById("level");
+	if(!element){
+		alert("Element with id 'level' not found.");
+	    return;
+	}
+
+	let currentElement=element;
+	let level=0;
+
+	while(currentElement){
+		level++;
+        currentElement = currentElement.parentElement;
+	}
+	alert(`The level of the element is: ${level}`);
+}
+
+findDOMLevel();
+
